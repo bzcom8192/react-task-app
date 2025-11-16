d22b3a511a32caabd1318487c4560ca4da8bb567
bzcom8192 <bzcom8192@gmail.com>
2025-11-16 09:41:43 +0700

    Add exact prop to route definitions for improved routing accuracy

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/showalltask" element={<ShowAllTask />} />
-        <Route path="/addtask" element={<AddTask />} />
-        <Route path="/updatetask/:id" element={<UpdateTask />} />
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/showalltask" exact={true} element={<ShowAllTask />} />
+        <Route path="/addtask" exact={true} element={<AddTask />} />
+        <Route path="/updatetask/:id" exact={true} element={<UpdateTask />} />
       </Routes>
     </BrowserRouter>
   )
